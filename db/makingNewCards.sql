5e221deae99a65fcc63550f37dc79d6e030bcbe5
Will Cruse <willcruse123@gmail.com>
2017-05-15 14:34:15 +0100

    Testing querys to see if DB design will work

--- db/makingNewCards.sql
+++ db/makingNewCards.sql
@@ -1,2 +1,5 @@
 USE educationWebsite;
-INSERT INTO cards (userOwn, term1, term2)values(1, "Hello", "World");
+INSERT INTO cards (userOwn) values(1);
+INSERT INTO terms VALUES(12, "Hello", "World");
+INSERT INTO terms VALUES(12, "print", "A function which outputs data to the console");
+SELECT * FROM terms WHERE setID = 12;
