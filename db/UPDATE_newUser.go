f4a3d6d9c31e69d0085a890e4db38b34439b36de
Will Cruse <willcruse123@gmail.com>
2017-05-16 11:46:51 +0100

    updated code to work on laptop and added passwords in comment

--- db/UPDATE_newUser.go
+++ db/UPDATE_newUser.go
@@ -1,33 +1,34 @@
 package main
 
-import(
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-  "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
-func main()  {
-    email := "somemail@address.com"
-    uName := "theREALguy"
-    pw := "UNHACKABLE"
-    db, err := sql.Open("mysql", "root:roottoor@/educationWebsite")
-    checkError(err)
-    defer db.Close()
-    errCon := db.Ping()
-    checkError(errCon)
-    stmt, err := db.Prepare("INSERT INTO users (email, uName, pw) VALUES(?, ?, ?)")
-    checkError(err)
-    res, err := stmt.Exec(email, uName, pw)
-    checkError(err)
-    affect, err := res.RowsAffected()
-    checkError(err)
-    fmt.Println("Rows:", affect)
+func main() {
+	email := "somemail@address.com"
+	uName := "theREALguy"
+	pw := "UNHACKABLE"
+	db, err := sql.Open("mysql", "will:somePass@/educationWebsite") //will:somePass   root:roottoor
+	checkError(err)
+	defer db.Close()
+	errCon := db.Ping()
+	checkError(errCon)
+	stmt, err := db.Prepare("INSERT INTO users (email, uName, pw) VALUES(?, ?, ?)")
+	checkError(err)
+	res, err := stmt.Exec(email, uName, pw)
+	checkError(err)
+	affect, err := res.RowsAffected()
+	checkError(err)
+	fmt.Println("Rows:", affect)
 }
 
 func checkError(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
